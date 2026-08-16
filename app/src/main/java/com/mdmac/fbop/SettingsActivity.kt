8c9b7c07e23508af82880e7cae2076563bf85e86
mdmac1983 <mdmac1983@proton.me>
2026-08-16 14:47:42 -0400

    Create SettingsActivity.kt

---
+++ app/src/main/java/com/mdmac/fbop/SettingsActivity.kt
@@ -0,0 +1,21 @@
+package com.mdmac.fbop
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settingsFragmentContainer, SettingsFragment())
+                .commit()
+        }
+
+        title = "FBOP Settings"
+    }
+}
